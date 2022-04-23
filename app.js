de4b311c4d4f7109cee9f735b2b841b96a9df86c
HadiOryaniPr <HadiOryaniHadiOryani>
2022-04-23 13:10:50 +0430

    change some code

--- app.js
+++ app.js
@@ -3,5 +3,6 @@
 const newToast = document.getElementById("newToast");
 
 function toast() {
-    alert('if you like press ok and shake you phone');
+   let toast = new bootstrap.Toast(newToast);
+   toast.show();
 }
