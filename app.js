86ac6ff6fe0a9aa556413dc2a3a6a1bd0b275158
HadiOryaniPr <HadiOryaniHadiOryani>
2022-04-23 20:02:37 +0430

    add preloader

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 
 const socialMediaBnt = document.querySelector(".socialMedia");
 const modalCloseBtn = document.querySelector(".modal-close-btn");
-
+const banner = document.querySelector(".banner");
 socialMediaBnt.addEventListener("click", () => {
   const notifi = document.getElementById("Up").play();
 });
 
 modalCloseBtn.addEventListener("click", () => {
   const donwNotifi = document.getElementById("Down").play();
+});
+
+window.addEventListener("load", () => {
+  banner.style.visibility = "hidden";
 });
